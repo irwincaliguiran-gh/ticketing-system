3768cdadb82540c99c652254a3f7a41b662a5ef4
irwincaliguiran-gh <irwincaliguiran@gmail.com>
2025-10-02 09:25:46 +0800

    Delete client.js

--- client.js
+++
@@ -1,9 +0,0 @@
-function loadPage(name){
-  google.script.host.close(); // if using dialog
-  window.location = name + '.html';
-}
-
-function GET_PARAM(k){
-  const params = new URLSearchParams(window.location.search);
-  return params.get(k);
-}
